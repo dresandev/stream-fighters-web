ca54ad35cc675ff72ca6117c4d5660e960195378
Dresan <dresan.dev@gmail.com>
2025-08-15 14:30:30 -0500

    add web sitemap and robots.tst

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
-import vercel from "@astrojs/vercel"
 import { defineConfig, envField } from "astro/config"
+import vercel from "@astrojs/vercel"
+import sitemap from "@astrojs/sitemap"
 
 export default defineConfig({
+  site: "https://stream-fighters-web.vercel.app",
   output: "server",
   adapter: vercel(),
+  integrations: [sitemap()],
   env: {
     schema: {
       HOST_URL: envField.string({ context: "client", access: "public" }),
