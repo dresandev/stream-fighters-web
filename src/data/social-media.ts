896535e9c1fe3513635a26036e29e6c2bdb462bc
Dresan <dresan.dev@gmail.com>
2025-09-28 19:56:30 -0500

    latest content u know

---
+++ src/data/social-media.ts
@@ -0,0 +1,17 @@
+export const socialMedia = [
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/stream_fighters",
+    label: "Visitar el Instagram de Stream Fighters 4"
+  },
+  {
+    name: "Tiktok",
+    url: "https://www.tiktok.com/@streamfighters",
+    label: "Visitar el TikTok de Stream Fighters 4"
+  },
+  {
+    name: "Github",
+    url: "https://github.com/dresandev/stream-fighters-web",
+    label: "Visitar el GitHub de la web no oficial de Stream Fighters 4"
+  },
+]
--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -1,85 +1,52 @@
-export const sponsors = {
-  officialSponsors: [
-    {
-      name: "Stake Colombia",
-      logo: "/images/logos/sponsors/stake.png",
-      url: "https://stake.com.co/es/bienvenida/"
-    },
-    {
-      name: "Kick",
-      logo: "/images/logos/sponsors/kick.png",
-      url: "https://kick.com/"
-    },
-    {
-      name: "Pragmatic Play",
-      logo: "/images/logos/sponsors/pragmatic-play.png",
-      url: "https://www.pragmaticplay.com/es/"
-    },
-  ],
-  supportedBy: [
-    {
-      name: "Amper",
-      logo: "/images/logos/sponsors/amper.png",
-      url: "https://www.instagram.com/amper_co/"
-    },
-    {
-      name: "Honda motos",
-      logo: "/images/logos/sponsors/honda.png",
-      url: "https://motos.honda.com.co/"
-    },
-    {
-      name: "Aguardiente nectar",
-      logo: "/images/logos/sponsors/aguardiente-nectar.png",
-      url: "https://www.instagram.com/aguardientenectar_oficial/"
-    },
-    {
-      name: "Everlast",
-      logo: "/images/logos/sponsors/everlast.png",
-      url: "https://everlast.com.co/"
-    },
-    {
-      name: "Fuego burger",
-      logo: "/images/logos/sponsors/fuego-burger.png",
-      url: "https://domicilios.fuegoburger.co/"
-    },
-    {
-      name: "Drinko",
-      logo: "/images/logos/sponsors/drinko.png",
-      url: "https://drinko.com.co/"
-    },
-  ],
-  createdBy: [
-    {
-      name: "Spoongg",
-      logo: "/images/logos/sponsors/spoongg.png",
-      url: "https://www.spoongg.com/"
-    },
-    {
-      name: "Westcol",
-      logo: "/images/logos/sponsors/w.png",
-      url: "https://www.instagram.com/westcol/"
-    },
-  ],
-  organizedBy: [
-    {
-      name: "Stage eventos",
-      logo: "/images/logos/sponsors/stage-eventos.png",
-      url: "https://stageeventos.com.co/"
-    },
-    {
-      name: "Diomar garcia eventos",
-      logo: "/images/logos/sponsors/diomar-garcia-eventos.png",
-      url: "https://diomargarciaeventos.com/"
-    },
-    {
-      name: "Touring the world",
-      logo: "/images/logos/sponsors/touring-the-world.png",
-      url: "https://touringtheworld.com/"
-    },
-    {
-      name: "Coliseo medplus",
-      logo: "/images/logos/sponsors/coliseo-medplus.png",
-      url: "https://www.coliseomedplus.com/"
-    },
-  ]
-}
+export const sponsors = [
+  {
+    name: "Stake Colombia",
+    logo: "/images/logos/sponsors/stake.png",
+    url: "https://stake.com.co/es/bienvenida/"
+  },
+  {
+    name: "Kick",
+    logo: "/images/logos/sponsors/kick.png",
+    url: "https://kick.com/"
+  },
+  {
+    name: "Pragmatic Play",
+    logo: "/images/logos/sponsors/pragmatic-play.png",
+    url: "https://www.pragmaticplay.com/es/"
+  },
+  {
+    name: "Amper",
+    logo: "/images/logos/sponsors/amper.png",
+    url: "https://www.instagram.com/amper_co/"
+  },
+  {
+    name: "Honda motos",
+    logo: "/images/logos/sponsors/honda.png",
+    url: "https://motos.honda.com.co/"
+  },
+  {
+    name: "Aguardiente nectar",
+    logo: "/images/logos/sponsors/aguardiente-nectar.png",
+    url: "https://www.instagram.com/aguardientenectar_oficial/"
+  },
+  {
+    name: "Everlast",
+    logo: "/images/logos/sponsors/everlast.png",
+    url: "https://everlast.com.co/"
+  },
+  {
+    name: "Fuego burger",
+    logo: "/images/logos/sponsors/fuego-burger.png",
+    url: "https://domicilios.fuegoburger.co/"
+  },
+  {
+    name: "Drinko",
+    logo: "/images/logos/sponsors/drinko.png",
+    url: "https://drinko.com.co/"
+  },
+  {
+    name: "Dulcinea",
+    logo: "/images/logos/sponsors/dulcinea.png",
+    url: "https://discotecadulcinea.com/"
+  },
+]
